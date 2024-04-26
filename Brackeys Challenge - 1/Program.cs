facdb51b22ab0cdd54ba2da970351069fce1a656
Austin <Austin97Marshall@hotmail.com>
2024-04-26 14:58:08 -0400

    Finished the brackey challenge, no current plans to add more.

--- Brackeys Challenge - 1/Program.cs
+++ Brackeys Challenge - 1/Program.cs
@@ -4,7 +4,65 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Wonderful adventure in Consolevania..
+
+            Console.Title = "Text Adventure";
+
+           
+            Console.WriteLine("Hello Adventurer, welcome to the world of Consolevania\nDo you have a name?\n");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine();
+            Console.WriteLine("Oh what a wonderful name!\nWhat brings you to Consolevania?\nvacation or adventure?\n");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //Choice to be made
+
+            if (Console.ReadLine() == "vacation")
+            {
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine("well.. if you haven't noticed there is alot of dangerous creatures around here!\n" +
+                    "OH MY, BEHIND YOU!! A DRAGON!!!\n");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Before your vacation could even begin a dragons eats you");
+
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine("Adventure!? That is great news there are many creatures to slay and quest to go on!\n" +
+                                  "Let's get you sorted out... OH, BEHIND YOU! A DRAGON!!!!!!\n");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Before your adventure could even begin a dragons eats you");
+
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
